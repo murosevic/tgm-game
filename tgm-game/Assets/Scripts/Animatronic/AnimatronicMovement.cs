73e87169aa34fc9c8c411f50c15b692a9d92f50a
murosevic <murosevic@student.tgm.ac.at>
2023-04-28 09:07:28 +0200

    Changed anim to Umaar, Added More Animatronic Points

--- tgm-game/Assets/Scripts/Animatronic/AnimatronicMovement.cs
+++ tgm-game/Assets/Scripts/Animatronic/AnimatronicMovement.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WehrMovement : MonoBehaviour
+public class AnimatronicMovement : MonoBehaviour
 {
+    public Transform[] positions = new Transform[4];
+
+    public GameObject wehr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(wehr, positions[0]);
     }
 
     // Update is called once per frame
